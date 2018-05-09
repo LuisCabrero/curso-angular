af25375455e82e4dfdc6805e197254fbaa2859ae
LuisCabrero <100330021@alumnos.uc3m.es>
2018-05-09 22:30:43 +0200

    Rutas y componentes en app de una sola página

---
+++ singlePageApplication/src/app/app.routes.ts
@@ -0,0 +1,11 @@
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+
+const app_routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: '**', pathMatch: 'full', redirectTo: '' }
+];
+
+export const app_routing = RouterModule.forRoot(app_routes);
