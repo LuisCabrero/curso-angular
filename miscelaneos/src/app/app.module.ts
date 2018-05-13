fecad0c19d6a535c406c9d2362a5ab907bc33d21
LuisCabrero <100330021@alumnos.uc3m.es>
2018-05-13 13:03:45 +0200

    Pruebas2

--- miscelaneos/src/app/app.module.ts
+++ miscelaneos/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { CssComponent } from './components/css/css.component';
 import { ClasesComponent } from './components/clases/clases.component';
 import { ResaltadoDirective } from './directives/resaltado.directive';
 import { NgSwitchComponent } from './components/ng-switch/ng-switch.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,8 @@ import { NgSwitchComponent } from './components/ng-switch/ng-switch.component';
     CssComponent,
     ClasesComponent,
     ResaltadoDirective,
-    NgSwitchComponent
+    NgSwitchComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
