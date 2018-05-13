287f79847a3265062989665333db55e83860334f
LuisCabrero <100330021@alumnos.uc3m.es>
2018-05-13 11:29:00 +0200

    Pruebas scopes css

--- miscelaneos/src/app/components/ng-style/ng-style.component.ts
+++ miscelaneos/src/app/components/ng-style/ng-style.component.ts
@@ -4,13 +4,23 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-ng-style',
   template: `
 
-    <p style="font-size:10px;">
+    <p [style.fontSize.px]="size">
       Hola Mundo... esta es una etiqueta
     </p>
+
+    <button class="btn btn-primary" (click)="size = size + 5">
+      <i class="fa fa-plus"></i>
+    </button>
+
+    <button class="btn btn-primary" (click)="size = size - 5">
+      <i class="fa fa-minus"></i>
+    </button>
   `,
   styles: []
 })
 export class NgStyleComponent implements OnInit {
+
+  size:number = 20;
 
   constructor() { }
 
