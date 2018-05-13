326df167d5565808550b69e88cff02cda9ac1098
LuisCabrero <100330021@alumnos.uc3m.es>
2018-05-13 12:21:52 +0200

    Directiva personalizada

--- miscelaneos/src/app/components/clases/clases.component.ts
+++ miscelaneos/src/app/components/clases/clases.component.ts
@@ -9,6 +9,9 @@ export class ClasesComponent implements OnInit {
 
   alerta:string = "alert-danger"
 
+  loading:boolean = false;
+
+
   propiedades:Object = {
     danger:true
   }
@@ -16,6 +19,11 @@ export class ClasesComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ejecutar(){
+    this.loading = true;
+    setTimeout(()=> this.loading = false , 3000);
   }
 
 }
