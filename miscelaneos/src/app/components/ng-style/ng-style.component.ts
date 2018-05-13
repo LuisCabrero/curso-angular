325a23288415a500027e0f66457747c94e5d4468
LuisCabrero <100330021@alumnos.uc3m.es>
2018-05-13 11:14:14 +0200

    Pruebas con directivas

---
+++ miscelaneos/src/app/components/ng-style/ng-style.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ng-style',
+  template: `
+
+    <p style="font-size:10px;">
+      Hola Mundo... esta es una etiqueta
+    </p>
+  `,
+  styles: []
+})
+export class NgStyleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
